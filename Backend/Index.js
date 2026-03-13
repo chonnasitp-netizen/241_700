da3f02d419820febf563cd71c4511ca1b612a3e1
chonnasitp-netizen <chonnasit.p@ku.th>
2026-03-13 18:07:31 +0700

    Add files via upload

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,97 +1,3 @@
-/*const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const { use, memo } = require('react');
-const mysql = require('mysql2/promise')
-app.use(bodyParser.json())
-
-
-
-const port = 8000;
-const initMySQL = async () => {
-    conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'webdb',
-        port: 8700
-    })
-    console.log('Connected to MySQL database');
-}
-
-app.get('/users', async (req, res) => {
-    const results = await conn.query('SELECT * FROM users');
-    res.json(results[0]);
-});
-
-app.post('/users', async (req, res) => {
-    try {
-        let user = req.body;
-        const results = await conn.query('INSERT INTO users SET ?', user);
-        res.json({
-            message: 'User added successfully',
-            data: results[0]
-        })
-    } catch (error) {
-        console.error('Error inserting user', error);
-        res.status(500).json({ message: 'Error adding user' });
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-    try {
-        let id = req.params.id;
-        const results = await conn.query('SELECT * FROM users WHERE id = ?', id);
-        if (results[0].length === 0) {
-            return res.status(404).json({ message: 'User not found' })
-        }
-        res.json(results[0][0]);
-    } catch (error) {
-        console.error('Error fetching user:'.error);
-        let statussCode = error.statussCode || 500;
-        res.status(statussCode).json({
-            message: error.message || 'Error fetching user'
-        })
-    }
-});
-
-app.put('/users/:id',async(req, res)=>{
-    try {
-        let id = req.params.id;
-        let updateUser = req.body;
-        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser, id])
-        res.json({
-            message: 'User deleted successfully',
-            data: results[0]
-            });
-    } catch (error) {
-        console.error('Error updated user:', error)
-        res.status(500).json({ message: 'Error updated user' });
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        let id = req.params.id;
-        const results = await conn.query('DELETE FROM users WHERE id=?', id)
-        res.json({
-            message: 'User deleted successfully',
-            data: results[0]
-        })
-    } catch (error) {
-        console.error('Error deleting user:', error);
-        res.status(500).json({ message: 'Error deleting user' });
-    }
-});
-
-app.listen(port, async () => {
-    await initMySQL();
-    console.log(`Sever is running on http://localhost:${port}`)
-});*/
-
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
@@ -126,18 +32,54 @@ app.get('/users', async (req, res) => {
     }
 });
 
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstName) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจ');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบาย');
+    }
+    return errors;
+}
+
 // POST /users - เพิ่มผู้ใช้ใหม่
 app.post('/users', async (req, res) => {
     try {
         let user = req.body;
+        const errors = validateData(user);
+        //เช็ค error โยนสคริปไปขว้าง
+        if (errors.length > 0){
+            throw {
+                message :  'กรุณากรอกข้อมูลให้ครอบถ้วน' ,
+                errors : errors
+            }
+        }
         const results = await conn.query('INSERT INTO users SET ?', user);
         res.json({
             message: 'User added successfully',
             data: results[0]
         });
     } catch (error) {
+        const errorMessage = error.message || 'Error adding user';
+        const errors = error.error || {};
         console.error('Error inserting user:', error);
-        res.status(500).json({ message: 'Error adding user' });
+        res.status(500).json({ 
+            message:errorMessage,
+            errors: errors
+        });
     }
 });
 
